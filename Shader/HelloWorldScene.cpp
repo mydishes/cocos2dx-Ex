1989dad02b91d400fa39248c535f0c7ad13d2689
Stephen <xifunlei@qq.com>
2017-10-16 16:41:43 +0800

    shader应用例子

---
+++ Shader/HelloWorldScene.cpp
@@ -0,0 +1,99 @@
+#include "HelloWorldScene.h"
+#include "RemindShader.h"
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                origin.y + closeItem->getContentSize().height/2));
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    
+    // position the label on the center of the screen
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
+
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+
+	m_pShader = RemindShader::create();
+	this->addChild(m_pShader);
+    // add "HelloWorld" splash screen"
+	m_pNode = Sprite::create("HelloWorld.png");
+
+    // position the sprite on the center of the screen
+	m_pNode->setPosition(Vec2(visibleSize.width / 2 -200, visibleSize.height/2 + origin.y));
+
+	auto pMove1 = MoveBy::create(1, Vec2(400, 0));
+	auto pMove2 = MoveBy::create(1, Vec2(-400, 0));
+	auto pSequenceMove = Sequence::createWithTwoActions(pMove1, pMove2);
+	auto pRepeatMove = RepeatForever::create(pSequenceMove);
+	m_pNode->runAction(pRepeatMove);
+    // add the sprite as a child to this layer
+	m_pShader->addChild(m_pNode, 0);
+
+    return true;
+}
+
+void HelloWorld::draw(Renderer *renderer, const Mat4 &transform, uint32_t flags)
+{
+	if (m_pShader)
+	{
+		m_pShader->push2Draw(m_pNode);
+	}
+}
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
---
+++ Shader/HelloWorldScene.h
@@ -0,0 +1,25 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+#include "RenderShader.h"
+class HelloWorld : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+
+	virtual void draw(Renderer *renderer, const Mat4 &transform, uint32_t flags) override;
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(HelloWorld);
+private:
+	RenderShader* m_pShader;
+	Node* m_pNode;
+};
+
+#endif // __HELLOWORLD_SCENE_H__
